b62c39d71dab3ccaf0b8d8783103bb4195f23f01
syjilv <visualimpulse@gmail.com>
2016-05-25 17:36:37 +0900

    160525 insert, update

--- springjdbc/src/myemp/MyEmpDAOImpl.java
+++ springjdbc/src/myemp/MyEmpDAOImpl.java
@@ -19,20 +19,23 @@ public class MyEmpDAOImpl implements MyEmpDAO {
 
 	@Override
 	public void insert(MyEmpDTO user) {
-		// TODO Auto-generated method stub
-		
+		String sql = "insert into myemp values(?, ?, ?, 1000, ?, '001', ?)";
+		int result = template.update(sql, user.getId(), user.getPass(), user.getAddr(), user.getGrade(), user.getName());
+		System.out.println(result + " Record(s) 삽입 성공");
 	}
 
 	@Override
 	public void update(MyEmpDTO userInfo) {
-		// TODO Auto-generated method stub
-		
+		String sql = "update myemp set id = ?, pass = ?, addr = ?, grade = ? where id = ?";
+		int result = template.update(sql, userInfo.getId(), userInfo.getPass(), userInfo.getAddr(), userInfo.getGrade(), userInfo.getId());
+		System.out.println(result + " Record 수정 성공");
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
-		
+		String sql = "delete from myemp where id = ?";
+		int result = template.update(sql, id);
+		System.out.println(result + ". " + id + " 삭제 성공");
 	}
 
 	@Override
